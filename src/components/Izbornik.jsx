560913ede603e73dc708f083a30786b1a81e27ff
draganzivkovic <drgzivko123@gmail.com>
2026-09-22 19:57:08 +0200

    napravljena navigacija fishijada i home

--- src/components/Izbornik.jsx
+++ src/components/Izbornik.jsx
@@ -2,43 +2,72 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
-import { IME_APLIKACIJE } from '../constants';
+import { IME_APLIKACIJE, RouteNames } from '../constants';
+import { useNavigate } from 'react-router-dom';
 
-
-
-
-
-//* vezemo Izbornik
 export default function Izbornik() {
 
-
+    const navigate = useNavigate();
 
     return (
         <Navbar expand="lg" className="bg-body-tertiary">
             <Container>
                 <Navbar.Brand href="#home">
                     {IME_APLIKACIJE}
-                    </Navbar.Brand>
+                </Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
-                        <Nav.Link href="#home">Početna</Nav.Link>
+                        <Nav.Link
+                            onClick={() => navigate(RouteNames.HOME)}
+                        >
+                            Početna
+                        </Nav.Link>
                         <NavDropdown title="Fishijade" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.1">Fishijada 2019.</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.1">Fishijada 2020.</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.1">Fishijada 2021.</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.1">Fishijada 2022.</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.1">Fishijada 2023.</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.1">Fishijada 2024.</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.1">Fishijada 2025.</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.1">Fishijada 2026.</NavDropdown.Item>
-                            
+                            <NavDropdown.Item
+                                onClick={() => navigate(RouteNames.FISHIJADE, { state: { godina: 2019 } })}
+                            >
+                                Fishijada 2019.
+                            </NavDropdown.Item>
+                            <NavDropdown.Item
+                                onClick={() => navigate(RouteNames.FISHIJADE, { state: { godina: 2020 } })}
+                            >
+                                Fishijada 2020.
+                            </NavDropdown.Item>
+                            <NavDropdown.Item
+                                onClick={() => navigate(RouteNames.FISHIJADE, { state: { godina: 2021 } })}
+                            >
+                                Fishijada 2021.
+                            </NavDropdown.Item>
+                            <NavDropdown.Item
+                                onClick={() => navigate(RouteNames.FISHIJADE, { state: { godina: 2022 } })}
+                            >
+                                Fishijada 2022.
+                            </NavDropdown.Item>
+                            <NavDropdown.Item
+                                onClick={() => navigate(RouteNames.FISHIJADE, { state: { godina: 2023 } })}
+                            >
+                                Fishijada 2023.
+                            </NavDropdown.Item>
+                            <NavDropdown.Item
+                                onClick={() => navigate(RouteNames.FISHIJADE, { state: { godina: 2024 } })}
+                            >
+                                Fishijada 2024.
+                            </NavDropdown.Item>
+                            <NavDropdown.Item
+                                onClick={() => navigate(RouteNames.FISHIJADE, { state: { godina: 2025 } })}
+                            >
+                                Fishijada 2025.
+                            </NavDropdown.Item>
+                            <NavDropdown.Item
+                                onClick={() => navigate(RouteNames.FISHIJADE, { state: { godina: 2026 } })}
+                            >
+                                Fishijada 2026.
+                            </NavDropdown.Item>
                         </NavDropdown>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
         </Navbar>
-
-    )
-
+    );
 }
--- src/constants.js
+++ src/constants.js
@@ -3,7 +3,7 @@ export const IME_APLIKACIJE= 'Fishijada'  //ovo je varijabla string
 
 export const RouteNames = {  // ovo je objekt - json
 HOME: '/',
-SMJEROVI: '/smjerovi',
+FISHIJADE: '/fishijade',
 
 
 }
